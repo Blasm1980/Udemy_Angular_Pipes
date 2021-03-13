0368ddc66d6d1070e35fa42c0973c80e3e13f5dd
Blasm1980 <blasm1980@gmail.com>
2021-03-13 22:57:00 +0100

    137

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'Blas mUndo ';
+  nombre: string = 'Blas Martinez ';
 
   mostrarNombre() {
     console.log( this.nombre );
