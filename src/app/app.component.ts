3618ffb355e74a3e464c2a1a8e3be4d7d46b13dc
Blasm1980 <blasm1980@gmail.com>
2021-03-14 12:50:59 +0100

    147

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   nombre: string = 'Blas Martinez ';
+
+  constructor( private primeNGConfig: PrimeNGConfig) {
+
+  }
 
   mostrarNombre() {
     console.log( this.nombre );
+  }
+
+  ngOnInit() {
+    this.primeNGConfig.ripple = true;
   }
 
 }
